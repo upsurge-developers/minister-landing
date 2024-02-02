99751a2a7e67c97f146cf97569c8cb106b82168e
Nelson Mbau <mbau.ndungu@gmail.com>
2024-02-02 18:58:26 +0300

    init: initialize app, add tailwind and shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import { cssBundleHref} from "@remix-run/css-bundle"
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -9,8 +9,11 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css"
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [])
 ];
 
 export default function App() {
--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,6 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  postcss: true,
+  tailwind: true
 };
